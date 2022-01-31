9a4158b6bc41073e93569177dbde66a4c67bf75f
CAW-94 <carysannwilliams@outlook.com>
2022-01-31 19:36:48 +0000

    returning to basic session creation

--- s3tospark.py
+++ s3tospark.py
@@ -1,42 +1,27 @@
-# import findspark
-
-# findspark.init()
-
-# import multiprocessing
-
-# import pyspark
-
-# cfg = (
-#     pyspark.SparkConf()
-#     # Setting where master node is located [cores for multiprocessing]
-#     .setMaster(f"local[{multiprocessing.cpu_count()}]")
-#     # Setting application name
-#     .setAppName("Pintrest_Test")
-#     # Setting config value via string
-#     .set("spark.eventLog.enabled", False)
-#     # Setting environment variables for executors to use
-#     .setExecutorEnv(pairs=[("VAR3", "value3"), ("VAR4", "value4")])
-#     # Setting memory if this setting was not set previously
-#     .setIfMissing("spark.executor.memory", "1g")
-# )
-
-# # Getting a single variable
-# print(cfg.get("spark.executor.memory"))
-# # Listing all of them in string readable format
-# print(cfg.toDebugString())
-
-# session = pyspark.sql.SparkSession.builder.config(conf=cfg).getOrCreate()
-
-# df = session.read
-
-import os
 import findspark
+import multiprocessing
 import pyspark
-from pyspark.sql import SparkSession
-spark = SparkSession
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = "--packages=org.apache.hadoop:hadoop-aws:2.7.3 pyspark-shell"
+
 findspark.init()
 
-access_id=
-access_key=
+cfg = (
+    pyspark.SparkConf()
+    # Setting where master node is located [cores for multiprocessing]
+    .setMaster(f"local[{multiprocessing.cpu_count()}]")
+    # Setting application name
+    .setAppName("PinterestTestApp")
+    # Setting config value via string
+    .set("spark.eventLog.enabled", False)
+    # Setting environment variables for executors to use
+    .setExecutorEnv(pairs=[("VAR3", "value3"), ("VAR4", "value4")])
+    # Setting memory if this setting was not set previously
+    .setIfMissing("spark.executor.memory", "1g")
+)
+
+# Getting a single variable
+print(cfg.get("spark.executor.memory"))
+# Listing all of them in string readable format
+print(cfg.toDebugString())
+
+
